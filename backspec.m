64d7138b89c4563b5770cbd88609cfbd095ba89b
kstdev <>
2003-02-13 12:34:38 +0000

    Moved from mean to median/mean

--- backspec.m
+++ backspec.m
@@ -5,10 +5,13 @@ bacf=zeros((maxlag+1),ncal);
 for i=1:ncal
  add=back(i)+1; len=nsamp(i)-1;
  for lag=0:maxlagb
-  bacf(lag+1,i)=mean(dd_data(add:(add+len)));
+  bacf(lag+1,i)=meddan(dd_data(add:(add+len)));
   add=add+len+1; len=len-1;
  end
 end
 h=spitspec(fig,ax,ones(maxlag+1,1)*(tsys(1:ncal)./bacf(1,:)).*bacf,(0:maxlag)*lagincr);
 set(h,'string','Background')
 if ncal>1, bacf=mean(bacf')'; end
+
+function m=meddan(x)
+m=mean(x(find(abs(x-median(x))<3*std(x))));
