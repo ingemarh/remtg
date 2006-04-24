5183d3f2e64c088042adabac1050bafed9dc94a9
kstdev <>
2006-04-24 12:12:20 +0000

    Added repetition routine

---
+++ def_rep.m
@@ -0,0 +1,13 @@
+function varargout=def_rep(mc,mr,j,varargin)
+%routine to repeat defs for different blocks
+for varno=1:length(varargin)
+ vartemp=varargin{varno};
+ varname=inputname(varno+3);
+ varargout{varno}=repmat(vartemp,mc,mr);
+ if strmatch(varname,{'txs' 'psig' 'back' 'cal' 'sig' 'sig0' 'backspec'},'exact')
+  s=ones(size(vartemp));
+  s=s(:)*(1:mr*mc);
+  [a,b]=size(varargout{varno})
+  varargout{varno}=varargout{varno}+j*reshape(s-1,b,a).';
+ end
+end
