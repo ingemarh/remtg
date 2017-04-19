da15b5f80f7f42a4064cecd76b6cead5854dd56f
ingemar <ingemar@lean.eiscat.se>
2017-04-19 11:48:27 +0200

    Helvetica-Narrow default font

--- rtg_startup.m
+++ rtg_startup.m
@@ -10,13 +10,18 @@ try
  if local.ver>=1, local.ver=0.9+local.ver/100; end
  local.ver=local.ver+str2num(matver);
  local.name=matvers(1).Name;
- if strcmp(local.name,'Octave') || local.ver<8.4
-  groot=0;
- else
-  set(groot,'defaultAxesTitleFontSizeMultiplier',1)
-  set(groot,'defaultAxesLabelFontSizeMultiplier',1)
-  set(groot,'defaultAxesTitleFontWeight','normal')
- end
+catch
+ local.ver=3; local.name='Octave'; local.x=0;
+ set(0,'defaultTextFontName','dejavu')
+end
+if strcmp(local.name,'Octave') || local.ver<8.4
+ groot=0;
+else
+ set(groot,'defaultAxesTitleFontSizeMultiplier',1)
+ set(groot,'defaultAxesLabelFontSizeMultiplier',1)
+ set(groot,'defaultAxesTitleFontWeight','normal')
+end
+if local.ver>3.4
  set(groot,'defaultAxesFontSize',12)
  set(groot,'defaultFigureMenuBar','none')
  set(groot,'defaultFigureNumberTitle','off')
@@ -28,14 +33,13 @@ try
  set(groot,'defaultUicontrolFontSize',10)
  set(groot,'defaultAxesxMinorTick','on')
  set(groot,'defaultAxesyMinorTick','on')
+ set(groot,'defaultTextFontName','Helvetica-Narrow')
+ set(groot,'defaultAxesFontName','Helvetica-Narrow')
  d=get(groot,'ScreenSize');
  local.x=prod(d)-1;
- if ~usejava('jvm') && local.ver>=7.5
+ if local.ver>=7.5 && ~usejava('jvm')
   set(groot,'DefaultAxesButtonDownFcn','zoom')
  end
-catch
- local.ver=3; local.name='Octave'; local.x=0; groot=0;
- set(0,'defaultTextFontName','dejavu')
 end
 set(groot,'defaultTextFontSize',12)
 set(groot,'defaultAxesColorOrder',[1 0 0;0 1 0;0 0 1;0 0 0;1 0 1;0 1 1;1 1 0;.5 .5 .5])
