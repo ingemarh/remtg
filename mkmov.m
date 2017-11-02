bc353b5a9f5dccb2fa141ca939fbcf4e9c733b6a
ingemar <ingemar@eiscat.se>
2017-11-02 15:05:57 +0100

    mencoder as a video maker option

--- mkmov.m
+++ mkmov.m
@@ -1,7 +1,8 @@
 function odir=mkmov(pldirs,ndumps)
 prog='ffmpeg';
-if unix(['which ' prog]), prog='avconv'; end
-if ~isempty(pldirs) && ~unix(['which ' prog]) && ~unix([prog ' -v error -buildconf | grep enable-libvpx'])
+if unix(['which ' prog]) || unix([prog ' -v error -buildconf | grep enable-libvpx']), prog='mencoder'; end
+if unix(['which ' prog]), return, end
+if ~isempty(pldirs)
  np=length(pldirs);
  rate=3; if ndumps<30, rate=10; end
  [dpath,name,ext]=fileparts(pldirs{1});
@@ -11,7 +12,13 @@ if ~isempty(pldirs) && ~unix(['which ' prog]) && ~unix([prog ' -v error -buildco
  for fn=f'
   [path,name,ext]=fileparts(fn.name);
   if strcmp(ext,'.png')
-   [dum1,dum2]=unix(sprintf('%s -r %d -pattern_type glob -i ''%s/*/%s.png'' -v error -vcodec libvpx -qmax 50 -y -dn -an %s/%s.webm',prog,rate,dpath,name,odir,name));
+   if strcmp(prog,'ffmpeg')
+    [dum1,dum2]=unix(sprintf('%s -r %d -pattern_type glob -i ''%s/*/%s.png'' -v error -vcodec libvpx -qmax 50 -y -dn -an %s/%s.webm',prog,rate,dpath,name,odir,name));
+   else
+    d=pwd; cd(dpath); unix(sprintf('ls */%s.png > list.txt',name));
+    [dum1,dum2]=unix(sprintf('mencoder mf://@list.txt -mf fps=%d:type=png -ovc copy -oac copy -o %s/%s.avi',rate,odir,name));
+    delete('list.txt'); cd(d)
+   end
    for i=1:np
     delete(fullfile(pldirs{i},fn.name));
    end
