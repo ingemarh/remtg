521aaaf249d5b3b457d5344e52902f694e946dd7
Carl-Fredrik Enell <carl-fredrik.enell@eiscat.se>
2019-03-05 15:24:14 +0100

    Sample script for starting the docker

---
+++ run_docker.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+### EISCAT rtg software / data overview animation
+### (C) Ingemar Häggström ingemar@eiscat.se
+###
+### Docker version
+### Carl-Fredrik Enell 2019 carl-fredrik.enell@eiscat.se
+###
+### Usage example for running the Docker container version
+##########################################################
+
+DOCKER_IMAGE=eiscat/remtg_volume:latest
+
+# 1. Copy the data to the input directory
+# Here are example data for testing:
+DATA_DIR=$PWD/test-input
+
+# 2. Create a directory for output
+OUT_DIR=$HOME/tmp/remtg
+
+# Run the container
+# docker pull $DOCKER_IMAGE
+docker run --rm --name remtg --mount type=bind,source=$DATA_DIR,destination=/srv/data --mount type=bind,source=$OUT_DIR,destination=/var/tmp $DOCKER_IMAGE
+
+
