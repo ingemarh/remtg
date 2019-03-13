a1a7e3736bb668cd0169d29aeb1644033e1fff8f
Carl-Fredrik Enell <carl-fredrik.enell@eiscat.se>
2019-03-13 13:47:41 +0100

    Data directories on host as arguments, with defaults

--- run_docker.sh
+++ run_docker.sh
@@ -12,10 +12,17 @@ DOCKER_IMAGE=eiscat/remtg_volume:latest
 
 # 1. Copy the data to the input directory
 # Here are example data for testing:
-DATA_DIR=$PWD/test-input
+# DATA_DIR=$PWD/test-input
+#
+DATA_DIR=$1
+[ -z $DATA_DIR] && DATA_DIR=$PWD/test-input
 
-# 2. Create a directory for output
-OUT_DIR=$HOME/tmp/remtg
+#2. Create a directory for output
+# Example
+# OUT_DIR=$HOME/tmp/remtg
+#
+OUT_DIR=$2
+[ -z $OUT_DIR] && OUT_DIR=$HOME/tmp/remtg
 
 # Run the container
 # docker pull $DOCKER_IMAGE
