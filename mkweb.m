4791fd27d7240766d4847ecd415f8c7906e3c583
t49851 <eiscat@t49851.eiscat.uit.no>
2019-09-23 10:33:37 +0000

    tromso additions

--- mkweb.m
+++ mkweb.m
@@ -8,11 +8,9 @@ else
 end
 sitex=lower(sitecode.mini(site));
 dir=local.tempdir;
-files='';
 files=fullfile(dir,[sitex '.html']);
 fid=fopen(files,'w');
-fprintf(fid,['<head>\n<TITLE>EISCAT-%s Real Time Graph</TITLE>\' ...
-             'n</head>\n'],char(sitecode.web(site)));
+fprintf(fid,'<head>\n<TITLE>EISCAT-%s Real Time Graph</TITLE>\n</head>\n',char(sitecode.web(site)));
 reloadfix=fix(10000*rand(1));
 for i=sort(figs)
  ffig=[sitex num2str(i)];
@@ -31,15 +29,13 @@ for i=sort(figs)
      print(i,['-d' jpg],'-noui',flag,fname)
     end
     files=[files ' ' fname '.png'];
-    fprintf(fid,'<IMG SRC="%s?%d" ALT="%s"><P>\n',[ffig '.png'], ...
-            reloadfix,get(i,'name'));
+    fprintf(fid,'<IMG SRC="%s?%d" ALT="%s"><P>\n',[ffig '.png'],reloadfix,get(i,'name'));
    else
     set(0,'currentfigure',i)
     fname=[fname '.png'];
     print(fname,'-dpng')
     files=[files ' ' fname];
-    fprintf(fid,'<IMG SRC="%s?%d"><P>\n',[ffig '.png'], ...
-            reloadfix);
+    fprintf(fid,'<IMG SRC="%s?%d"><P>\n',[ffig '.png'],reloadfix);
    end
   catch
    disp(lasterr)
@@ -77,9 +73,9 @@ if site==6
   unix(['cp ' files ' /net/aurora/www/ht/rtg/']);
  end
 end
-%if site==4
-%unix(['scp -q ' files ' palver:/var/www/rtg/']); 
-%end
+if site==4 || site==3
+ unix(['scp -q ' files ' palver5:/var/www/html/rtg/']); 
+end
 % RTG upload to www.eiscat.se
 [i,d]=unix('ps | grep curl | grep -v grep');
 if i
