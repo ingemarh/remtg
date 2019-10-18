b691fd9ddfdc43cd0153efa5874115bb5ed9d73a
Carl-Fredrik Enell <carl-fredrik.enell@eiscat.se>
2019-10-18 16:45:32 +0200

    minor thing

--- mkmov.m
+++ mkmov.m
@@ -7,6 +7,7 @@ if ~isempty(pldirs)
  rate=3; if ndumps<30, rate=10; end
  [dpath,t2,ext]=fileparts(pldirs{end});
  [dpath,t1,ext]=fileparts(pldirs{1});
+ [d,t0a,t0b]=fileparts(dpath);
  odir=sprintf('%s_%s_%d',dpath,t1,np);
  mkdir(odir);
  f=dir(pldirs{1});
@@ -14,7 +15,7 @@ if ~isempty(pldirs)
   [path,name,ext]=fileparts(fn.name);
   if strcmp(ext,'.png')
    if strcmp(prog,'ffmpeg')
-    [dum1,dum2]=unix(sprintf('%s -r %d -pattern_type glob -i ''%s/*/%s.png'' -metadata title="%s %s %s %d" -v error -vcodec libvpx -qmax 50 -y -dn -an %s/%s.webm',prog,rate,dpath,name,dpath,t1,t2,np,odir,name));
+    [dum1,dum2]=unix(sprintf('%s -r %d -pattern_type glob -i ''%s/*/%s.png'' -metadata title="%s%s %s %s %d" -v error -vcodec libvpx -qmax 50 -y -dn -an %s/%s.webm',prog,rate,dpath,name,t0a,t0b,t1,t2,np,odir,name));
    else
     unix(sprintf('find %s -name %s.png > list.txt',dpath,name));
     [dum1,dum2]=unix(sprintf('mencoder mf://@list.txt -mf fps=%d:type=png -ovc lavc -o %s/%s.avi',rate,odir,name));
