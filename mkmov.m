0c5c7164d1e93efb12daa7c389c9bcbaae1ac7ff
Ingemar <ingemar@lean.eiscat.se>
2019-10-18 15:07:28 +0200

    Add metadata to movie

--- mkmov.m
+++ mkmov.m
@@ -5,18 +5,19 @@ if ~isempty(pldirs)
  if unix(['which ' prog]), return, end
  np=length(pldirs);
  rate=3; if ndumps<30, rate=10; end
- [dpath,name,ext]=fileparts(pldirs{1});
- odir=sprintf('%s_%s_%d',dpath,name,np);
+ [dpath,t2,ext]=fileparts(pldirs{end});
+ [dpath,t1,ext]=fileparts(pldirs{1});
+ odir=sprintf('%s_%s_%d',dpath,t1,np);
  mkdir(odir);
  f=dir(pldirs{1});
  for fn=f'
   [path,name,ext]=fileparts(fn.name);
   if strcmp(ext,'.png')
    if strcmp(prog,'ffmpeg')
-    [dum1,dum2]=unix(sprintf('%s -r %d -pattern_type glob -i ''%s/*/%s.png'' -v error -vcodec libvpx -qmax 50 -y -dn -an %s/%s.webm',prog,rate,dpath,name,odir,name));
+    [dum1,dum2]=unix(sprintf('%s -r %d -pattern_type glob -i ''%s/*/%s.png'' -metadata title="%s %s %s %d" -v error -vcodec libvpx -qmax 50 -y -dn -an %s/%s.webm',prog,rate,dpath,name,dpath,t1,t2,np,odir,name));
    else
     unix(sprintf('find %s -name %s.png > list.txt',dpath,name));
-    [dum1,dum2]=unix(sprintf('mencoder mf://@list.txt -mf fps=%d:type=png -ovc copy -oac copy -o %s/%s.avi',rate,odir,name));
+    [dum1,dum2]=unix(sprintf('mencoder mf://@list.txt -mf fps=%d:type=png -ovc lavc -o %s/%s.avi',rate,odir,name));
     delete('list.txt');
    end
    for i=1:np
